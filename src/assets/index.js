74d48b2d87474e4d3f16b7e93474b85c5583e64f
Shasatya <120493084+Shasatya@users.noreply.github.com>
2024-07-28 21:37:18 +0530

    added responsiveness

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,65 +1,75 @@
+import logo from "../assets/logo.png";
+import star from "../assets/star.png";
+
+//project
 import p1 from "../assets/1.png";
 import p2 from "../assets/2.png";
 import p3 from "../assets/3.png";
 import p4 from "../assets/4.png";
 import p5 from "../assets/5.png";
 
-import logo from "../assets/logo.png";
-import star from "../assets/star.png";
+//stack 1
+import js from "../assets/js.png";
+import cpp from "../assets/cpp.png";
+import python from "../assets/python.png";
 
+//stack 2
 import html from "../assets/html.png";
 import css from "../assets/css.png";
-import js from "../assets/js.png";
-import bootstrap from "../assets/bootstrap.png";
+import scss from "../assets/scss.png";
 import react from "../assets/react.png";
-import gsap from "../assets/gsap.png";
-import kotlin from "../assets/kotlin.png";
-import python from "../assets/python.png";
-import java from "../assets/java.png";
-import node from "../assets/node.png";
-import vs from "../assets/vs.png";
-import mysql from "../assets/mysql.png";
-import mongo from "../assets/mongo.png";
-import express from "../assets/express.png";
-import jquery from "../assets/jquery.png";
-import astudio from "../assets/astudio.png";
-import idea from "../assets/idea.png";
-import postman from "../assets/postman.png";
+import rn from "../assets/rn.png";
+import tail from "../assets/tail.svg";
+import nativeW from "../assets/nativeW.png";
 import chakra from "../assets/chakra.png";
-import canva from "../assets/canva.png";
-import redux from "../assets/redux.png";
-import insomnia from "../assets/insomnia.png";
+import bootstrap from "../assets/bootstrap.png";
 import figma from "../assets/figma.png";
 
+//stack 3
+import node from "../assets/node.png";
+import express from "../assets/express.png";
+import appWrite from "../assets/appWrite.png";
+
+//stack 4
+import mongo from "../assets/mongo.png";
+import mysql from "../assets/mysql.png";
+
+//stack 5
+import vs from "../assets/vs.png";
+import idea from "../assets/idea.png";
+import insomnia from "../assets/insomnia.png";
+import postman from "../assets/postman.png";
+import canva from "../assets/canva.png";
+
 export {
+  logo,
+  star,
   p1,
   p2,
   p3,
   p4,
   p5,
-  logo,
-  star,
-  insomnia,
-  redux,
-  canva,
-  chakra,
-  postman,
-  astudio,
-  idea,
-  express,
-  jquery,
-  mysql,
-  mongo,
+  js,
+  cpp,
+  python,
   html,
   css,
-  js,
-  bootstrap,
+  scss,
   react,
-  gsap,
-  kotlin,
-  python,
-  java,
-  node,
-  vs,
+  rn,
+  tail,
+  nativeW,
+  chakra,
+  bootstrap,
   figma,
+  node,
+  express,
+  appWrite,
+  mongo,
+  mysql,
+  vs,
+  idea,
+  insomnia,
+  postman,
+  canva,
 };
